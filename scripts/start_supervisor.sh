7941c662cca33900747a02240c7db7e47532647c
henok-tesfu <henoktesf48@gmail.com>
2025-01-28 23:20:08 +0300

    script file

---
+++ scripts/start_supervisor.sh
@@ -0,0 +1,3 @@
+docker exec myapp supervisorctl reread
+docker exec myapp supervisorctl update
+docker exec myapp supervisorctl start
---
+++ scripts/swagger_doc_generate.sh
@@ -0,0 +1,1 @@
+docker-compose run --rm artisan l5-swagger:generate
